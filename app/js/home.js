9c8232fbff082214d9a88ceed07fd73117ffa4c8
Voicu Apostol <cerpow@gmail.com>
2021-07-23 22:25:29 +0300

    Fix autofocus error.

--- app/js/home.js
+++ app/js/home.js
@@ -13,7 +13,7 @@ async function Home(selectIndex) {
 		html += '<div class="projects-search">';
 		html +=
 			'<div class="projects-search-input-wrapper"><svg xmlns="http://www.w3.org/2000/svg" width="20" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>';
-		html += '<input type="text" spellcheck="false" placeholder="Search..." autofocus>';
+		html += '<input type="text" spellcheck="false" placeholder="Search..." >';
 		html +=
 			'<svg class="projects-search-reset hidden" width="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg></div>';
 		html +=
